61f83a2185dbaab0bb268dcff89d003faf85064e
Sallah10 <107303495+Sallah10@users.noreply.github.com>
2025-04-22 21:19:56 +0100

    add button to alert message in index.html and initialize script.js with basic functionality

---
+++ conference-ticket-generator-main/script.js
@@ -0,0 +1,59 @@
+//Variable
+let abdulssalam = "Azeemat";
+// var abdurrahman = "Abdurrahman";
+abdulssalam = "Abdulssalam";
+
+//constant
+const sonic = "Sonic";
+sonic = "Sonic the Hedgehog";
+
+// Data Types:
+// String === text, "string"
+// number== 1, 1.3 
+// boolean == true, false
+
+// +, -, *, /
+let firstNumber = 10;
+let secondNumber = 5;
+let sum = firstNumber * secondNumber;
+firstNumber % secondNumber;
+
+firstNumber === secondNumber; // false
+firstNumber = secondNumber;	//5
+firstNumber !== secondNumber; // true
+
+function checkTrue(firstNumber, secondNumber) {
+    return firstNumber === secondNumber;
+}
+
+function checkfalse(firstNumber, secondNumber) {
+    return firstNumber !== secondNumber;
+}
+
+function checkequal(firstNumber, secondNumber) {
+    return firstNumber = secondNumber;
+}
+
+greet("Ayo");
+
+if (age < 13) {
+    console.log("You're a kid!");
+} else {
+    console.log("You're a teen or older!");
+}
+
+if (age < 13) {
+    console.log("You're a kid!");
+} else if (age < 20) {
+    console.log("You're a teen!");
+}
+else if (age < 65) {
+    console.log("You're an adult!");
+} else {
+    console.log("You're a senior citizen!");
+}
+
+
+
+console.log('your sum of numbers is: ', sum);
+
