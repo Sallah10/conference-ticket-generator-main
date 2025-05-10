50814ba8e44f810714a346921e3754b60e918e5e
Sallah10 <107303495+Sallah10@users.noreply.github.com>
2025-05-10 12:40:47 +0100

    Add ticket generation functionality with form handling

--- conference-ticket-generator-main/script.js
+++ conference-ticket-generator-main/script.js
@@ -64,3 +64,47 @@ else if (age < 65) {
 
 console.log('your sum of numbers is: ', sum);
 
+function generateTicket() {
+    const form = document.getElementById("form");
+    const nameInput = document.getElementById("nameInput");
+    const github = document.getElementById("git-hub");
+
+    // Get the ticket section and elements
+    //   const ticket = document.getElementById("ticket");
+    const ticketGithub = document.getElementById("ticket-git-hub");
+    const ticketName = document.getElementById("ticketName");
+    const ticketId = document.getElementById("ticketId");
+
+    // When the form is submitted
+    form.addEventListener("submit", function (e) {
+        e.preventDefault(); // Stop the page from refreshing
+
+        const name = nameInput.value;
+
+
+
+        if (name.trim() === "") {
+            alert("Please enter your name.");
+            return;
+        }
+
+        // Show the name
+        ticketName.textContent = name;
+        ticketGithub.textContent = github.value
+
+        // Create a random ticket ID
+        const randomId = "TICKET-" + Math.floor(100000 + Math.random() * 900000);
+
+        // Show the ticket ID
+        ticketId.textContent = randomId;
+
+        // // Show the ticket section
+        // ticket.style.display = "block";
+
+        // Clear the input
+        nameInput.value = "";
+    });
+}
+
+// Call the function to set everything up
+generateTicket();
